b998595736840464a0643a2415b952848404eed1
seongjun lee <rooneylee@outlook.com>
2018-07-27 23:00:43 -0700

    [FEAT] xml builder from summary of parsed webpage * Rename : XmlGenerator.js -> xmlGenerator.js

---
+++ xmlGenerator.js
@@ -0,0 +1,17 @@
+const xml = require("xml");
+
+const generateXml = (target, summary) => {
+  summary.unshift(
+    { title: target.feed.title },
+    { link: target.feed.link },
+    { description: target.feed.description },
+    { pubDate: new Date().toGMTString() }
+  );
+
+  // let xmlComplete = xml({ rss: summary }, { declaration: true }, true);
+  let xmlComplete = xml(
+    { rss: [{ channel: summary }, { _attr: { version: "2.0" } }] },
+    true
+  );
+  console.log(xmlComplete);
+};

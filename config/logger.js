8e1350629c4acd2494bd695e1f446af8983ade68
seongjun lee <rooneylee@outlook.com>
2018-08-16 14:28:35 -0700

    [FEAT] logger implementaion

---
+++ config/logger.js
@@ -0,0 +1,24 @@
+const { createLogger, format, transports } = require("winston");
+require("winston-daily-rotate-file");
+const fs = require("fs");
+
+const logDir = "log";
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const dailyRotateFileTransport = new transports.DailyRotateFile({
+  filename: `${logDir}/%DATE%-results.log`,
+  datePattern: "YYYY-MM-DD"
+});
+
+const logger = createLogger({
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [new transports.Console(), dailyRotateFileTransport]
+});
+
+module.exports = logger;

742ba3295d44cf032613a26692bb222fcb953bae
seongjun lee <rooneylee@outlook.com>
2018-08-19 23:24:31 -0700

    [FEAT] mongodb and logging implementation

--- App.js
+++ App.js
@@ -1,36 +1,38 @@
-const spawn = require("child_process").spawn;
 const express = require("express");
 const app = express();
 
-const parseTarget = require("./targetParser");
 const feedGenerator = require("./feedGenerator");
+const mongodb = require("./config/dbClient.js");
+const logger = require("./config/logger.js");
 
 const port = 12121;
-const path = "/feed/";
+const path = __dirname + "/feed/";
 
-const parseWeb = () => {
-  return new Promise(resolve => {
-    const pyProg = spawn("python3", ["webScraper.py"]);
-    pyProg.stdout.on("data", () => {
-      resolve();
-    });
+const startFeedBuilder = async () => {
+  // mongodb connection
+  try {
+    await mongodb.connect();
+    logger.info("DB - connection established");
+  } catch (err) {
+    logger.error("DB - connection failed");
+    logger.error(err);
+    process.exit(1);
+  }
+
+  // server initialization
+  app.get("/:id", (request, response) => {
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    logger.info("EXP - GET - '" + ip + "' -> " + request.params.id);
+    response.set("Content-Type", "text/xml; charset=uft-8");
+    response.sendFile(path + request.params.id);
+    logger.info("EXP - R - '" + ip + "' -> " + path + request.params.id);
   });
+
+  app.listen(port);
+  logger.info("EXP - server is listening " + port);
+
+  // start feed building
+  feedGenerator();
 };
 
-const startApp = () => {
-  parseWeb()
-    .then(parseTarget)
-    .then(feedGenerator);
-};
-
-app.get("/:id", (request, response) => {
-  console.log(
-    "REQUEST ::: FROM - " + request.params.id + " FOR - " + request.params.id
-  );
-  response.set("Conthent-Type", "text/xml");
-  response.sendFile(__dirname + path + request.params.id);
-});
-
-startApp();
-setInterval(startApp, 600000);
-app.listen(port);
+startFeedBuilder();

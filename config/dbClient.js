ada4e2c21f28c0a1ce935d7e5a85ec9d909ecdff
seongjun lee <rooneylee@outlook.com>
2018-08-19 23:23:58 -0700

    [FEAT] mongodb implementation

--- config/dbClient.js
+++ config/dbClient.js
@@ -1,17 +1,57 @@
 const MongoClient = require("mongodb").MongoClient;
+const reqlib = require("app-root-path").require;
+const logger = reqlib("/config/logger");
+
+const url = "mongodb://localhost:27017";
+const collectionList = "collectionList";
+const dbName = "feedBuilder";
+
 let _db;
 
-const connect = cb => {
-  MongoClient.connect(
-    "mongodb://localhost:27017/feedBuilder",
-    { useNewUrlParser: true },
-    (err, db) => {
-      _db = db;
-      return cb(err);
-    }
-  );
+const connect = () => {
+  const parser = { useNewUrlParser: true };
+  const client = new MongoClient(url, parser);
+  return new Promise(resolve => {
+    client.connect(err => {
+      if (err) {
+        reject(err);
+      }
+      _db = client.db(dbName);
+      resolve(_db);
+    });
+  });
 };
 
-const getDB = () => _db;
+const collection = async target => {
+  const { title, link, description } = target.feed;
 
-module.exports = { connect, getDB };
+  try {
+    const targetCollection = _db.collection(collectionList);
+    const setTargetQuery = { $set: { link, description } };
+    const upsert = true;
+    await targetCollection.updateOne({ title }, setTargetQuery, { upsert });
+    logger.info("DB - collectionList updated -> " + title);
+  } catch (err) {
+    logger.error("DB - collectionList update failed -> " + title);
+    throw err;
+  }
+
+  const targetCollection = _db.collection(title);
+  const currentSize = await targetCollection.find().count();
+  if (currentSize == 0) {
+    logger.info("DB - collection[" + title + "] does not exist");
+    logger.info("DB - creating capped collection[" + title + "]");
+    const capped = { capped: true, size: 100000, max: 100 };
+    try {
+      await _db.createCollection(title, capped);
+      logger.info("DB - collection[" + title + "] created");
+    } catch (err) {
+      logger.error("DB - failed to create collection[" + title + "]");
+      throw err;
+    }
+  }
+};
+
+const getDB = collection => _db.collection(collection);
+
+module.exports = { connect, collection, getDB };

e2b48a8f19445e05e0591a733c704c7e0265fa55
Seongjun Lee <rooneylee@outlook.com>
2018-08-26 15:22:27 -0700

    [FIX] target specific item parsing

--- feedCollector.js
+++ feedCollector.js
@@ -1,47 +1,57 @@
 const rp = require("request-promise");
 const cheerio = require("cheerio");
 const logger = require("./config/logger");
-const parseHtml = require("./htmlParser");
 const parseTarget = require("./targetParser");
 const mongodb = require("./config/dbClient.js");
 
-const buildFeed = async target => {
-  for (url of target.feed.urls) {
-    try {
-      logger.info("FEED - requesting -> " + url);
-      const html = await rp(url);
-      logger.info("FEED - requesting '" + url + "'" + "success");
-      const decode = { decodeEntities: false };
-      const $ = cheerio.load(html, decode);
+const parseHtml = async ($, target) => {
+  const dedicatedParser = require("./target/" + target.id);
+  const item = await dedicatedParser($);
 
-      // Divide html dom into each story section
-      // the story section is sent to htmlParser
-      // htmlParser extract nessary information and store in db
-      logger.info("FEED - redirecting '" + url + "' to htmlParse");
-      $(target.content.root)
-        .find(target.content.section)
-        .each((index, ele) => parseHtml($(ele), target));
-    } catch (err) {
-      logger.error("FEED - error occured while requesting web data");
-      logger.error("FEED -       from -> " + url);
-      logger.error(err);
+  if (item) {
+    const db = mongodb.getDB(target.id);
+    const exist = await db.find({ guid: item.guid }).count();
+
+    if (exist == 0) {
+      db.insertOne(item);
     }
   }
 };
 
-const generateFeed = async () => {
+const buildFeed = async target => {
+  try {
+    const html = await rp(target.url);
+    const decode = { decodeEntities: false };
+    const $ = cheerio.load(html, decode);
+
+    // Divide html dom into each story section
+    // the story section is sent to htmlParser
+    // htmlParser extract nessary information and store in db
+    $(target.root)
+      .find(target.section)
+      .each((index, ele) => parseHtml($(ele), target));
+  } catch (err) {
+    logger.error("FEED - error occured while requesting web data");
+    logger.error("FEED -       from -> " + target.url);
+    logger.error(err);
+  }
+};
+
+const collectFeed = async () => {
   logger.info("FEED - starting feedGenerator");
   try {
     const targets = parseTarget();
     for (target of targets) {
-      logger.info("FEED - processing '" + target.feed.title + "'");
+      logger.info("FEED - processing '" + target.id + "'");
       await mongodb.collection(target);
       buildFeed(target);
     }
   } catch (err) {
     logger.error("FEED - error while running feedGenerator");
-    throw err;
+    logger.error(err);
   }
+
+  setTimeout(collectFeed, 3600000);
 };
 
-module.exports = generateFeed;
+module.exports = collectFeed;

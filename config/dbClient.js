c0eb7cb12828823dad493bb9a5623b2ce583b539
Seongjun Lee <login@sjlee.org>
2018-08-30 16:53:30 -0700

    [FIX] mongodb data id signiture change

--- config/dbClient.js
+++ config/dbClient.js
@@ -23,31 +23,33 @@ const connect = () => {
 };
 
 const collection = async target => {
-  const { title, link, description } = target.feed;
   const pubDate = new Date().toGMTString();
 
+  const { title, link, description } = target;
   try {
     const targetCollection = _db.collection(collectionList);
-    const setTargetQuery = { $set: { link, description, pubDate } };
+    const setTargetQuery = { $set: { title, link, description, pubDate } };
     const upsert = true;
-    await targetCollection.updateOne({ title }, setTargetQuery, { upsert });
-    logger.info("DB - collectionList updated -> " + title);
+    await targetCollection.updateOne({ _id: target.id }, setTargetQuery, {
+      upsert
+    });
+    logger.info("DB - collectionList updated -> " + target.id);
   } catch (err) {
-    logger.error("DB - collectionList update failed -> " + title);
+    logger.error("DB - collectionList update failed -> " + target.id);
     throw err;
   }
 
-  const targetCollection = _db.collection(title);
+  const targetCollection = _db.collection(target.id);
   const currentSize = await targetCollection.find().count();
   if (currentSize == 0) {
-    logger.info("DB - collection[" + title + "] does not exist");
-    logger.info("DB - creating capped collection[" + title + "]");
+    logger.info("DB - collection[" + target.id + "] does not exist");
+    logger.info("DB - creating capped collection[" + target.id + "]");
     const capped = { capped: true, size: 524288, max: 50 };
     try {
-      await _db.createCollection(title, capped);
-      logger.info("DB - collection[" + title + "] created");
+      await _db.createCollection(target.id, capped);
+      logger.info("DB - collection[" + target.id + "] created");
     } catch (err) {
-      logger.error("DB - failed to create collection[" + title + "]");
+      logger.error("DB - failed to create collection[" + target.id + "]");
       throw err;
     }
   }
